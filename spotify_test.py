5ca46318ed63116a9c7fbe010b8218db7d29885e
RoopansheeMaharana <roopanhsee@gmail.com>
2026-03-12 18:06:11 +0530

    Update spotify_test.py

--- spotify_test.py
+++ spotify_test.py
@@ -1,8 +1,8 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_id = "b96e07ca10a64e8aa6e8d39c0e5d3088"
-client_secret = "430ada2fc1ed48d080a2852bd1561d08"
+client_id = "YOUR_CLIENT_ID"
+client_secret = "YOUR_CLIENT_SECRET"
 
 auth_manager = SpotifyClientCredentials(
     client_id=client_id,
@@ -12,5 +12,6 @@ auth_manager = SpotifyClientCredentials(
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
 results = sp.search(q="Shape of You", type="track", limit=1)
+
 
 print(results)
